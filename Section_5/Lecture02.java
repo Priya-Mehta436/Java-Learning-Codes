544f7f56cabe1c6f017a513297d61a6f9db56399
Priya Mehta <pmehta12122005@gmail.com>
2025-06-24 09:11:28 +0530

    Statement_Whitespace

---
+++ Section_5/Lecture02.java
@@ -0,0 +1,25 @@
+package Section_5;
+
+public class Lecture02 {
+     public static void main(String[] args) {
+          
+          
+          int myvariable = 50;
+          myvariable--;
+          myvariable++;
+
+
+          System.out.println("This is a test");
+          System.out.println("This is just " + 
+          "another " + 
+          "Test");
+
+          
+          System.out.println
+          (
+               "the variable is " + myvariable
+               )
+               ;
+          int anotherVariable = 50; myvariable--; System.out.println("my variable " + myvariable + " and another variable " + anotherVariable);
+     }
+}
