cb746fbfd1332f9b9b03d6a9c47248c42adb3815
Priya Mehta <pmehta12122005@gmail.com>
2025-06-27 20:44:38 +0530

    POJOs

--- Section_7/L13_POJO.java
+++ Section_7/L13_POJO.java
@@ -4,7 +4,7 @@ package Section_7;
 public class L13_POJO{
     public static void main(String[] args) {
         for(int i = 1; i<=5 ; i++){
-            POJO_2 student = new POJO_2 ( "J2071" + i,
+            L14_POJO_Records student = new L14_POJO_Records ( "J2071" + i,
             switch(i){
                 case 1 -> "Sian";
                 case 2 -> "Carrie";
@@ -17,5 +17,21 @@ public class L13_POJO{
             "06/06/2003", "masters");
             System.out.println(student);
         }
+         POJO_2 pojoStudent = new POJO_2("788806", "Den",
+                "15/01/1965", "Java Masterclass");
+        L14_POJO_Records recordStudent = new L14_POJO_Records("J70007", "Lara",
+                "25/05/2001", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        pojoStudent.setClassList(pojoStudent.getClassList() + ", Java OCP Exam 829");
+        //recordStudent.setClassList(recordStudent.classList() + ", Java OCP Exam 829");
+
+        System.out.println(pojoStudent.getName() + " is taking " +
+                pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " +
+                recordStudent.classList());
     }
-  }
+}
+
---
+++ Section_7/L14_POJO_Records.java
@@ -0,0 +1,6 @@
+package Section_7;
+
+public record L14_POJO_Records(String id, String name, String dateOfBirth, String classList) {
+    
+}
+    
