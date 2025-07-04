40def35ab03a8e7621b689e23f7649c75a8b40c1
Priya Mehta <pmehta12122005@gmail.com>
2025-07-04 14:13:38 +0530

    Polymorphism Challenge

---
+++ Section_8/Polymorphism/Challenge/Main.java
@@ -0,0 +1,21 @@
+package Section_8.Polymorphism.Challenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car car = new Car("2022 Blue Ferrari 296 GTS");
+        runRace(car);
+
+        Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS",
+                15.4, 6);
+        runRace(ferrari);
+    }
+
+    public static void runRace(Car car) {
+
+        car.startEngine();
+        car.drive();
+    }
+}
+
