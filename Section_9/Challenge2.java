4d9f4dd58be8d36980c931c8a1e3b72812770125
Priya Mehta <pmehta12122005@gmail.com>
2025-07-05 20:25:20 +0530

    Swapping Elements

--- Section_9/Challenge2.java
+++ Section_9/Challenge2.java
@@ -8,8 +8,16 @@ public class Challenge2 {
         int[] returnedArray = readIntegers();
         System.out.println(Arrays.toString(returnedArray));
 
-        int returnedMin = findMin(returnedArray);
-        System.out.println("min = " + returnedMin);
+        // int returnedMin = findMin(returnedArray);
+        // System.out.println("min = " + returnedMin);
+
+        //    reverse(returnedArray);
+        //    System.out.println("Final: " + Arrays.toString(returnedArray));
+
+        int[] reversedCopy = reverseCopy(returnedArray);
+        System.out.println("After reverse " + Arrays.toString(returnedArray));
+        System.out.println("rversedCopy " + Arrays.toString(reversedCopy));
+
     }
 
     private static int[] readIntegers() {
@@ -37,5 +45,26 @@ public class Challenge2 {
             }
         }
         return min;
+    }
+    private static void reverse (int[] array){
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for(int i = 0; i < halfLength; i++){
+            int temp = array[i];
+            array [i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+            System.out.println("--->" + Arrays.toString(array));
+        }
+
+    }
+    private static int [] reverseCopy(int[] array){
+        int [] reverseArray = new int[array.length];
+        int maxIndex = array.length - 1;
+        for(int el : array){
+            reverseArray[maxIndex--] = el;
+        }
+        return reverseArray;
+
     }
 }
